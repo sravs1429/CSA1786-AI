c6a9ab6eebe42d941f085ae0f8b9169a3bfae77c
sravs1429 <113814412+sravs1429@users.noreply.github.com>
2023-02-06 08:37:31 +0530

    Create move.pl

---
+++ move.pl
@@ -0,0 +1,20 @@
+move(1,X,Y,_) :-
+         write('Move top disk from '),
+         write(X),
+         write(' to '),
+         write(Y),
+nl.
+     move(N,X,Y,Z) :-
+         N>1,
+         M is N-1,
+         move(M,X,Z,Y),
+         move(1,X,Y,_),
+         move(M,Z,Y,X).american(robert).
+enemy(countryA).
+sells_weapons(robert, countryA).
+has_missiles(countryA).
+
+criminal(X) :- american(X), sells_weapons(X, Y), enemy(Y).
+Footer
+Footer
+© 2023 GitHub, Inc.
